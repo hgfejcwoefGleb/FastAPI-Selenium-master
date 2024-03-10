0357871f077b9ba39cc53f811025cdfa9eb014a2
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-10 20:33:04 +0300

    ghhj

--- main.py
+++ main.py
@@ -33,52 +33,37 @@ async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
     background_tasks.add_task(doBackgroundTask, inp)
     return {"message": "Success, background task started"}
 """
+class Request(BaseModel):
+    meta: dict
+    request: dict
+    session: dict
+    version: str
 
+
+class Response(BaseModel):
+    response: dict
+    session: dict
+    version: str
 
 @app.post("/alice_skill")
-def alice_skill():
-    return {"response": {
-    "text": "Здравствуйте! Это мы, хороводоведы.",
-    "tts": "Здравствуйте! Это мы, хоров+одо в+еды.",
-    "card": {
-      "type": "...",
-    },
-    "buttons": [
-        {
-            "title": "Надпись на кнопке",
-            "payload": {},
-            "url": "https://example.com/",
-            "hide": True
-        }
-    ],
-    "end_session": False,
-    "directives": {}
-  },
-  "session_state": {
-      "value": 10
-  },
-  "user_state_update": {
-      "value": 42
-  },
-  "application_state": {
-      "value": 37
-  },
-  "analytics": {
-        "events": [
-            {
-                "name": "custom event"
-            },
-            {
-                "name": "another custom event",
-                "value": {
-                    "field": "some value",
-                    "second field": {
-                        "third field": "custom value"
-                    }
-                }
-            }
-        ]
-    },
-  "version": "1.0"}
+def alice_skill(request: Request):
+    # Получение текста, введенного пользователем
+    user_text = request.request["command"]
+
+    # Формирование ответа
+    response_text = f"Вы сказали: {user_text}"
+    response = {
+        "text": response_text,
+        "end_session": False
+    }
+
+    # Формирование ответа для Яндекс.Алисы
+    alice_response = Response(
+        response=response,
+        session=request.session,
+        version=request.version
+    )
+
+    return alice_response
 
 
