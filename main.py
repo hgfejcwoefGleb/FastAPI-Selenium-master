4397f95ae9924cda1fc06a9ef9c2b1a2bc38b2f3
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-10 20:45:04 +0300

    ghhj

--- main.py
+++ main.py
@@ -3,70 +3,32 @@ from pydantic import BaseModel
 from extract import *
 import os
 
-
-
+SECRET = os.getenv("SECRET")
 
 #
 app = FastAPI()
 
-"""class Msg(BaseModel):
+
+class Msg(BaseModel):
     msg: str
     secret: str
 
-@app.get("/")
 
+@app.get("/")
 async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
 
 
 @app.get("/homepage")
 async def demo_get():
-    driver=createDriver()
+    driver = createDriver()
 
     homepage = getGoogleHomepage(driver)
     driver.close()
     return homepage
 
+
 @app.post("/backgroundDemo")
 async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
-    
     background_tasks.add_task(doBackgroundTask, inp)
     return {"message": "Success, background task started"}
-"""
-class Request(BaseModel):
-    meta: dict
-    request: dict
-    session: dict
-    version: str
-
-
-class Response(BaseModel):
-    response: dict
-    session: dict
-    version: str
-
-@app.post("/alice_skill")
-def alice_skill(event, context):
-    """
-    Entry-point for Serverless Function.
-    :param event: request payload.
-    :param context: information about current execution context.
-    :return: response to be serialized as JSON.
-    """
-    text = 'Hello! I\'ll repeat anything you say to me.'
-    if 'request' in event and \
-            'original_utterance' in event['request'] \
-            and len(event['request']['original_utterance']) > 0:
-        text = event['request']['original_utterance']
-    return {
-        'version': event['version'],
-        'session': event['session'],
-        'response': {
-            # Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
-            'text': text,
-            # Don't finish the session after this response.
-            'end_session': 'false'
-        },
-    }
-
-
