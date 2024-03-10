aeccd43395b63b0a26333094909367f31344bcd1
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-10 20:34:58 +0300

    ghhj

--- main.py
+++ main.py
@@ -46,24 +46,27 @@ class Response(BaseModel):
     version: str
 
 @app.post("/alice_skill")
-def alice_skill(request: Request):
-    # Получение текста, введенного пользователем
-    user_text = request.request["command"]
-
-    # Формирование ответа
-    response_text = f"Вы сказали: {user_text}"
-    response = {
-        "text": response_text,
-        "end_session": False
+def alice_skill(event, context):
+    """
+    Entry-point for Serverless Function.
+    :param event: request payload.
+    :param context: information about current execution context.
+    :return: response to be serialized as JSON.
+    """
+    text = 'Hello! I\'ll repeat anything you say to me.'
+    if 'request' in event and \
+            'original_utterance' in event['request'] \
+            and len(event['request']['original_utterance']) > 0:
+        text = event['request']['original_utterance']
+    return {
+        'version': event['version'],
+        'session': event['session'],
+        'response': {
+            # Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
+            'text': text,
+            # Don't finish the session after this response.
+            'end_session': 'false'
+        },
     }
-
-    # Формирование ответа для Яндекс.Алисы
-    alice_response = Response(
-        response=response,
-        session=request.session,
-        version=request.version
-    )
-
-    return alice_response
 
 
