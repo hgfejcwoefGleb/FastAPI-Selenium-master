2325bb4eef46db461ea64b52f24b87725b09fd81
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-17 17:53:39 +0300

    ghhj

--- main.py
+++ main.py
@@ -7,30 +7,29 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST']) #
-@app.route('/alice/', methods=['POST'])
-def respond():
-    data = request.json
-    command = data.get('request', {}).get('command', '')
-
-    end_session = False
-
-    if 'выход' in command:
-        response_text = 'До свидания!'
-        end_session = True
-    elif command:
-        response_text = f'Вы сказали {command}'
-    else:
-        response_text = 'Привет! Вы ничего не сказали.'
-
-    response = {
+@app.post("/alice")
+def handler(event, context):
+    """
+    Entry-point for Serverless Function.
+    :param event: request payload.
+    :param context: information about current execution context.
+    :return: response to be serialized as JSON.
+    """
+    text = 'Hello! I\'ll repeat anything you say to me.'
+    if 'request' in event and \
+            'original_utterance' in event['request'] \
+            and len(event['request']['original_utterance']) > 0:
+        text = event['request']['original_utterance']
+    return {
+        'version': event['version'],
+        'session': event['session'],
         'response': {
-            'text': response_text,
-            'end_session ': end_session
+            # Respond with the original request or welcome the user if this is the beginning of the dialog and the request has not yet been made.
+            'text': text,
+            # Don't finish the session after this response.
+            'end_session': 'false'
         },
-        'version': '1.0'
     }
-    return response
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
